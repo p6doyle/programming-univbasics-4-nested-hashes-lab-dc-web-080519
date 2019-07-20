afa47f3988575d05ae0868b4a522cda96a17bdbe
Peyton Doyle <p6doyle@gmail.com>
2019-07-20 10:38:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,12 +62,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-programmer_hash << ":yukihiro_matsumoto"
 
 	programmer_hash = 
  		{
@@ -85,6 +79,13 @@ programmer_hash << ":yukihiro_matsumoto"
         }
      }
 
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
+programmer_hash << :yukihiro_matsumoto
+return programmer_hash
     
 end
 
