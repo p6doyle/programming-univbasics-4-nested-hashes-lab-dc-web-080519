35bf82cfda9d892a45e87bcdff533d24e7737858
Peyton Doyle <p6doyle@gmail.com>
2019-07-20 10:54:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash << :yukihiro_matsumoto{}
+programmer_hash[:yukihiro_matsumoto]
 return programmer_hash
     
 end
